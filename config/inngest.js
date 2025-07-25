ddf517ec884021b9570826e68dda426daebd8847
Prateek Verma <prateek222rithik@gmail.com>
2025-07-25 17:21:05 +0530

    comment

--- config/inngest.js
+++ config/inngest.js
@@ -61,31 +61,31 @@ export const syncUserDeletion = inngest.createFunction(
 )
 
 // Inngest Function to create user's order in database
-export const createUserOrder = inngest.createFunction(
-    {
-        id: 'create-user-order',
-        batchEvents: {
-            maxSize: 5,
-            timeout: '5s'
-        }
-    },
-    { event: 'order/created' },
-    async ({ events }) => {
+// export const createUserOrder = inngest.createFunction(
+//     {
+//         id: 'create-user-order',
+//         batchEvents: {
+//             maxSize: 5,
+//             timeout: '5s'
+//         }
+//     },
+//     { event: 'order/created' },
+//     async ({ events }) => {
 
-        const orders = events.map((event) => {
-            return {
-                userId: event.data.userId,
-                items: event.data.items,
-                amount: event.data.amount,
-                address: event.data.address,
-                date: event.data.date
-            }
-        })
+//         const orders = events.map((event) => {
+//             return {
+//                 userId: event.data.userId,
+//                 items: event.data.items,
+//                 amount: event.data.amount,
+//                 address: event.data.address,
+//                 date: event.data.date
+//             }
+//         })
 
-        await connectDB()
-        await Order.insertMany(orders)
+//         await connectDB()
+//         await Order.insertMany(orders)
 
-        return { success: true, processed: orders.length };
+//         return { success: true, processed: orders.length };
 
-    }
-)
+//     }
+// )
